6dee0ca44266e5ceacb1981546ecff3fa62bda74
DavidCarrilho <luan.david.oliver@gmail.com>
2022-06-30 00:42:20 -0300

    chore: create splash and login pages

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
@@ -6,6 +5,20 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Container(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(Icons.person, size: MediaQuery.of(context).size.height * 0.2),
+            const TextField(decoration: InputDecoration(label: Text('Login'))),
+            const TextField(decoration: InputDecoration(label: Text('Senha')), obscureText: true),
+            const SizedBox(height: 15),
+            ElevatedButton(onPressed: () {}, child: const Text('LOGIN'))
+          ],
+        ),
+      ),
+    );
   }
 }
